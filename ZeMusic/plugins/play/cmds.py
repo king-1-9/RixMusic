f9c77030723dddf851adbc64012252407553131a
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-06-05 13:40:18 +0300

    Update cmds.py

--- ZeMusic/plugins/play/cmds.py
+++ ZeMusic/plugins/play/cmds.py
@@ -18,7 +18,7 @@ async def zdatsr(client: Client, message: Message):
     usrnam = usr.username
     await message.reply_photo(
         photo=START_IMG_URL,
-        caption=f"""<b>» منور يحبي باوامر الميوزك </b>\n\n<b>» استخـدم الازرار بالاسفـل 𝄞\n» لـ تصفـح اوامـر الميـوزك 🥁</b>""",
+        caption=f"""<b>⟡ منور يحبي باوامر الميوزك .</b>\n\n<b>⟡ استخدم الازرار الي تحت .\n⟡ عشان تشوف اوامر الميوزك يعيني .</b>""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
